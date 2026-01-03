22ba5df6ff1a0e1829261c4cbb8b02f2d5e4de05
SkidBoi <skid.fanning788@slmails.com>
2026-01-03 02:09:21 +0000

    Create not_found.js

---
+++ app/not_found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 40, fontFamily: "monospace" }}>
+      <h1>404</h1>
+      <p>wrong alley.</p>
+      <a href="/">go home</a>
+    </div>
+  );
+}
